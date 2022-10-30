f27ce8f8c2cb2571a75865bc50aa092d034ba16a
rodrigoSolano <rodrigosolano014@gmail.com>
2022-10-30 15:41:11 -0600

    feat: start server

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"log"
+
+	"github.com/rodrigoSolano/twitter_go/db"
+	"github.com/rodrigoSolano/twitter_go/handlers"
+)
+
 func main() {
-	
+	if !db.CheckConnection() {
+		log.Fatal("Connection to database failed")
+		return
+	}
+	handlers.Handlers()
 }
